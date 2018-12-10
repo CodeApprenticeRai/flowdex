796ca34f3d743b2154441620ea80d0e8ffb1f980
Tare <tare.dgg@gmail.com>
2018-12-09 19:48:46 -0500

    hello

---
+++ README.md
@@ -0,0 +1,5 @@
+# react-no-bloat-starter
+
+npm install, then npm start.
+
+No bloat, start building your components, and testing your React application. 
---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import {} from '../components';
+import "./../styleshsdeets/sheet1.css";
+
+
+class App extends Component {
+  render(){
+    return(
+      <div className='container-fluid'>
+        <p>Hello WTF!!!</p> 
+      </div>
+    );
+  }
+}
+
+export default App;
