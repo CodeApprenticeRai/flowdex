e3146c6f82b1c4df0b599e8f8f08f131ddae35a8
Tare <tare.dgg@gmail.com>
2018-12-09 20:00:10 -0500

    webpack you *****

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# react-no-bloat-starter
+# flowdex
 
-npm install, then npm start.
-
-No bloat, start building your components, and testing your React application. 
+shhh!!! 
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 // import {} from '../components';
-import "./../styleshsdeets/sheet1.css";
+// import "../stylesheets/sheet1.css";
+
 
 
 class App extends Component {
   render(){
     return(
       <div className='container-fluid'>
-        <p>Hello WTF!!!</p> 
+        Southern Sierra Wildflower Club Flower Index
       </div>
     );
   }
